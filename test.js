b8b096e49c5f2998e247f79f08c4e72c4ad9e0e0
Ryan Block <rblock@gmail.com>
2022-03-16 21:08:09 -0700

    Add ability to return chunked as object / unchunked as buffer

--- test.js
+++ test.js
@@ -6,6 +6,9 @@ let tmp = path.join(__dirname, 'tmp')
 let newDest = () => path.join(tmp, `${Date.now()}`)
 let baked = path.join(__dirname, 'baked.zip')
 let { chunk, unchunk } = require('.')
+
+let original = fs.readFileSync(src)
+let maxSize = 1000 * 1000 // 1MB
 let dest
 
 function reset () {
@@ -16,49 +19,87 @@ function reset () {
   fs.mkdirSync(dest, { recursive: true })
 }
 
-test('chunk (default size of 3MB)', async t => {
+test('Chunk (default size of 3MB, written to disk)', async t => {
   t.plan(1)
   dest = newDest()
   reset()
   await chunk({ src, dest })
   let chunks = fs.readdirSync(dest, { withFileTypes: true })
-  t.ok(chunks.length === 4, 'there are four chunks')
+  t.ok(chunks.length === 4, 'There are four chunks')
   console.log(chunks)
 })
 
-test('unchunk', async t => {
+test('Unchunk (default size of 3MB, written to disk)', async t => {
   t.plan(1)
   await unchunk({
     src: dest,
     dest: baked,
   })
-  let original = fs.readFileSync(src)
   let copy = fs.readFileSync(baked)
-  t.deepEqual(original, copy, 'Unchunked / rechunked files match')
+  t.deepEqual(original, copy, 'Unchunked file matches')
 })
 
-test('chunk (custom size of 1MB)', async t => {
+test('Unchunk (default size of 3MB, returned in buffer)', async t => {
+  t.plan(1)
+  let copy = await unchunk({
+    src: dest,
+    write: false,
+  })
+  t.deepEqual(original, copy, 'Unchunked buffer matches')
+})
+
+test('Chunk (default size of 3MB, returned in an object)', async t => {
+  t.plan(2)
+  dest = newDest()
+  reset()
+  let chunks = await chunk({ src, write: false })
+  t.equal(Object.keys(chunks).length, 4, 'There are four chunks')
+  let manuallyUnchunked = Buffer.concat(Object.values(chunks))
+  t.deepEqual(original, manuallyUnchunked, 'Unchunked / rechunked files match')
+  console.log(chunks)
+})
+
+test('Chunk (specified size of 1MB, written to disk)', async t => {
   t.plan(1)
   dest = newDest()
   reset()
-  await chunk({ src, dest, maxSize: (1000 * 1000) })
+  await chunk({ src, dest, maxSize })
   let chunks = fs.readdirSync(dest, { withFileTypes: true })
-  t.ok(chunks.length === 10, 'there are ten chunks')
+  t.ok(chunks.length === 10, 'There are ten chunks')
   console.log(chunks)
 })
 
-test('unchunk', async t => {
+test('Unchunk (specified size of 1MB, written to disk)', async t => {
   t.plan(1)
   await unchunk({
     src: dest,
     dest: baked,
   })
-  let original = fs.readFileSync(src)
   let copy = fs.readFileSync(baked)
-  t.deepEqual(original, copy, 'Unchunked / rechunked files match')
+  t.deepEqual(original, copy, 'Unchunked file matches')
 })
 
-test('teardown', t => {
+test('Unchunk (specified size of 1MB, returned in buffer)', async t => {
+  t.plan(1)
+  let copy = await unchunk({
+    src: dest,
+    write: false,
+  })
+  t.deepEqual(original, copy, 'Unchunked buffer matches')
+})
+
+test('Chunk (specified size of 1MB, returned in an object)', async t => {
+  t.plan(2)
+  dest = newDest()
+  reset()
+  let chunks = await chunk({ src, maxSize, write: false })
+  t.equal(Object.keys(chunks).length, 10, 'There are ten chunks')
+  let manuallyUnchunked = Buffer.concat(Object.values(chunks))
+  t.deepEqual(original, manuallyUnchunked, 'Unchunked / rechunked files match')
+  console.log(chunks)
+})
+
+test('Teardown', t => {
   t.plan(1)
   reset()
   t.pass('Done!')
