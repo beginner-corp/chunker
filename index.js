48ed4e388439e100fca1a09de56d3ead9ae03f04
Ryan Block <rblock@gmail.com>
2022-03-16 19:35:37 -0700

    Fix lint

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const CHUNK_MAX = 3e+6 // 3mb
 
 module.exports = { chunk, unchunk }
 
-async function chunk({src, dest}) {
+async function chunk ({ src, dest }) {
 
   // read src to a buffer
-  let raw = fs.readFileSync(src) 
+  let raw = fs.readFileSync(src)
   let shasum = crypto.createHash('sha1').update(raw.toString('base64'))
   let guid = shasum.digest('hex')
   let chunks = []
   let index = 0
 
   // iterate the bytes
-  for (const [i, byte] of raw.entries()) {
+  for (const [ , byte ] of raw.entries()) {
 
     // if the current chunk is full increment the index
     if (chunks[index] && chunks[index].length >= CHUNK_MAX)
@@ -28,9 +28,9 @@ async function chunk({src, dest}) {
     // push byte into chunk
     chunks[index].push(byte)
   }
-  
-  // chunk src into 3mb chunks 
-  let count = chunks.length 
+
+  // chunk src into 3mb chunks
+  let count = chunks.length
   for (let index = 0; index < count; index++) {
     let name = `${guid}-${index}-${count}`
     let b = Buffer.from(chunks[index])
@@ -38,12 +38,12 @@ async function chunk({src, dest}) {
   }
 }
 
-async function unchunk({src, dest}) {
-  let fmt = f=> path.join(src, f.name) 
+async function unchunk ({ src, dest }) {
+  let fmt = f => path.join(src, f.name)
   let chunks = fs.readdirSync(src, { withFileTypes: true }).map(fmt)
   let result = []
   for (let chunk of chunks)
-    for (const [i, b] of fs.readFileSync(chunk).entries())
+    for (const [ , b ] of fs.readFileSync(chunk).entries())
       result.push(b)
   fs.writeFileSync(dest, Buffer.from(result))
 }
